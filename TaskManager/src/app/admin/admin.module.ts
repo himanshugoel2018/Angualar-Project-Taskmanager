87427ac381fa86d363932c047328ef279d50bdac
hgoel <himanshu_goel@optum.com>
2020-02-02 22:26:36 +0530

    Move About C to Admin M

--- TaskManager/src/app/admin/admin.module.ts
+++ TaskManager/src/app/admin/admin.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
-import { AboutComponent } from '../about/about.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
