be3d3048efa01b50c33f89263caa49ead028e449
hgoel <himanshu_goel@optum.com>
2020-02-02 22:25:45 +0530

    Move myProfile C to Admin M

--- TaskManager/src/app/admin/admin.module.ts
+++ TaskManager/src/app/admin/admin.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MyProfileComponent } from '../my-profile/my-profile.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
 import { AboutComponent } from '../about/about.component';
 
 @NgModule({
