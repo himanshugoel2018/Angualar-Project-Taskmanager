39eb8a061bea5823443f763cb8170b337d2e8fc2
hgoel <himanshu_goel@optum.com>
2020-02-03 06:12:55 +0530

    Adding TaskManager API project

---
+++ TaskManagerAPI/TaskManagerAPI/Models/Project.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagerAPI.Models
+{
+	public class Project
+	{
+		[Key]
+		public int ProjectID { get; set; }
+		public string ProjectName { get; set; }
+		public DateTime DateOfStart { get; set; }
+		public int TeamSize { get; set; }
+	}
+
+	public class TaskManagerDbContext : DbContext
+	{
+		public DbSet<Project> Projects { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			optionsBuilder.UseSqlServer("data source=localhost; integrated security=yes; initial catalog = TaskManagerDatabase");
+		}
+	}
+}
