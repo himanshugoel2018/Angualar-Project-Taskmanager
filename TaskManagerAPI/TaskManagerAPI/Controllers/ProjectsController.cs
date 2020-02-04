2c364863dba5990b842ea887f7ab6bd0532ea7e2
hgoel <himanshu_goel@optum.com>
2020-02-04 07:17:31 +0530

    Creating GET method in project C

---
+++ TaskManagerAPI/TaskManagerAPI/Controllers/ProjectsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagerAPI.Models;
+
+namespace TaskManagerAPI.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        [HttpGet]
+        [Route("api/projects")]
+        public List<Project> Get()
+        {
+            TaskManagerDbContext db = new TaskManagerDbContext();
+            List<Project> projects = db.Projects.ToList();
+            return projects;
+        }
+    }
+}
