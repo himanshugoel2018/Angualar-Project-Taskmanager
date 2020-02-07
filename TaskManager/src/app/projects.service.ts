62cb85ef8d7ef7d78fb2f77e0b429025757a756c
hgoel <himanshu_goel@optum.com>
2020-02-07 07:13:21 +0530

    Get method in Project Service

--- TaskManager/src/app/projects.service.ts
+++ TaskManager/src/app/projects.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from './project';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
 
-  constructor() { }
+  constructor(private httpclient: HttpClient) { }
+
+  getAllProjects(): Observable<Project[]> {
+    return this.httpclient.get<Project[]>('/api/projects');
+  }
 }
