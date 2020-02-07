5f3d81a6b7560b1fb9d41a1c60caaa7fc6d530ce
hgoel <himanshu_goel@optum.com>
2020-02-07 06:53:36 +0530

    updating Taskmanager API

---
+++ TaskManagerAPI/TaskManagerAPI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagerAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- TaskManagerAPI/TaskManagerAPI/Models/Project.cs
+++ TaskManagerAPI/TaskManagerAPI/Models/Project.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TaskManagerAPI.Models
 {
 	public class Project
 	{
 		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
 		public int ProjectID { get; set; }
 		public string ProjectName { get; set; }
 		public DateTime DateOfStart { get; set; }
@@ -20,7 +22,7 @@ namespace TaskManagerAPI.Models
 		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 		{
 			base.OnConfiguring(optionsBuilder);
-			optionsBuilder.UseSqlServer("data source=localhost; integrated security=yes; initial catalog = TaskManagerDatabase");
+			optionsBuilder.UseSqlServer("data source=LHTU05CG8335W82\\SQLEXPRESS2014; integrated security=yes; initial catalog = TaskManager");
 		}
 	}
 }
