cc60addb10c2333c8868db41c9e6a6e18a176bdc
hgoel <himanshu_goel@optum.com>
2020-02-07 07:17:38 +0530

    Moving Project C to Admin Folder

--- TaskManager/src/app/app.module.ts
+++ TaskManager/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminModule } from './admin/admin.module';
-import { ProjectsComponent } from './projects/projects.component';
+import { ProjectsComponent } from './admin/projects/projects.component';
 
 @NgModule({
   declarations: [
