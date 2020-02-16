8c1850d326ccc201eb96cf4454b84238a0da7e4a
hgoel <himanshu_goel@optum.com>
2020-02-16 09:45:02 +0530

    usage of MAP operator in ANG

--- TaskManager/src/app/projects.service.ts
+++ TaskManager/src/app/projects.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Project } from './project';
 
 @Injectable({
@@ -12,7 +13,16 @@ export class ProjectsService {
 
   getAllProjects(): Observable<Project[]> {
     // return this.httpclient.get<Project[]>('/api/projects');
-    return this.httpclient.get<Project[]>('http://localhost:1762/api/projects');
+    return this.httpclient.get<Project[]>('http://localhost:1762/api/projects')
+      .pipe(map(
+        (data: Project[]) => {
+          for (let index = 0; index < data.length; index++) {
+            data[index].teamSize = data[index].teamSize * 100;
+
+          }
+          return data;
+        }
+      ));
   }
 
   insertProject(newProject: Project): Observable<Project> {
