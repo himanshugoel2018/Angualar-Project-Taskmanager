efdacbd222832c866ae96aae678e5044b05cc2ba
hgoel <himanshu_goel@optum.com>
2020-02-16 19:10:37 +0530

    Login Component APP + Authentication

--- TaskManager/src/app/app-routing.module.ts
+++ TaskManager/src/app/app-routing.module.ts
@@ -3,17 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 import { AboutComponent } from './admin/about/about.component';
 import { ProjectsComponent } from './admin/projects/projects.component';
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
+  { path: "", component: LoginComponent, pathMatch: "full" },
+  { path: "login", component: LoginComponent },
   { path: "dashboard", component: DashboardComponent },
   { path: "about", component: AboutComponent },
-  { path: "projects", component: ProjectsComponent },
-  { path: "", component: DashboardComponent, pathMatch: "full" }
+  { path: "projects", component: ProjectsComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ TaskManager/src/app/login.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoginViewModel } from './login-view-model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  currentUserName: string = null;
+
+  public Login(loginViewModel: LoginViewModel): Observable<any> {
+    return this.httpClient.post<any>("http://localhost:1762/authenticate", loginViewModel, { responseType: "json" })
+      .pipe(map(
+        (user) => {
+          if (user) {
+            this.currentUserName = user.userName;
+          }
+          return user;
+        }));
+  }
+
+  public Logout() {
+    this.currentUserName = null;
+  }
+}
